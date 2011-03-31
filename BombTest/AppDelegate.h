4a81fa42d4d01b064313cfa3b92fda00c3405961
Quinn Stephens <quinn@quinnstephens.com>
2011-03-31 17:50:57 -0500

    Initial Commit

---
+++ BombTest/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BombTest
+//
+//  Created by Quinn Stephens on 3/31/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
